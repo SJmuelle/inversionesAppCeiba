82e9f928a4f8887e39aaf8fb3febc31af1b88e90
SHIRLEY  MUELLE CAMARGO <62040229+SJmuelle@users.noreply.github.com>
2025-08-29 04:38:13 -0500

    Create CorsConfig.java

---
+++ src/main/java/com/btg/inversionesapp/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.btg.inversionesapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        // aquí puedes poner la URL de tu frontend en Netlify
+                        .allowedOrigins("https://<tu-frontend>.netlify.app", "http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
